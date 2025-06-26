1f002262185e175c2cb0b79f58ad76ca9c7a1677
Nemozikr <ebhomrah@gmail.com>
2025-06-26 11:49:14 +0100

    reconfigured hardcoded APIs in App.tsx and main.tsx to use import.meta.env to improve flexibility with development and production hosting

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -5,9 +5,9 @@ import './App.css'
 function App() {
   let [message, setMessage] = useState('')
   const fetchData = () => {
-    fetch('/api/hello-world')
-      .then(res => res.text())
-      .then(data => setMessage(data))
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/hello-world`)
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
   }
 
   return (
--- chat-client/src/main.tsx
+++ chat-client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-fetch('/api/alive')
+fetch(`${import.meta.env.VITE_API_BASE_URL}/alive`)
   .then(res => console.log(res.text()))
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
